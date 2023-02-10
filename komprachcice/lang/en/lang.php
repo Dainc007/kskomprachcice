38a0bbd37e57adb0e93b1fe91317b1e036d6082e
Daniel Heinze <daniel.he@modulesgarden.com>
2023-02-10 17:13:17 +0100

    langs

---
+++ komprachcice/lang/en/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'nav' => [
+        'howToSignIn' => 'How to sign in?',
+        'groups'      => 'Groups',
+        'coaches'     => 'Coaches',
+        'partners'    => 'Partners',
+        'tournaments' => 'Tournaments',
+        'galery'      => 'Galery',
+        'trainingGear' => 'Training Gear'
+    ],
+
+    'groups' => [
+        'youngest' => 'Youngest U7',
+        'young'    => 'Young U9',
+        'mid'      => 'Mid U11',
+        'old'      => 'Old U13',
+    ]
+];
---
+++ komprachcice/lang/pl/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'nav' => [
+        'howToSignIn' => 'Jak się zapisać?',
+        'groups'      => 'Grupy',
+        'coaches'     => 'Trenerzy',
+        'partners'    => 'Partnerzy',
+        'tournaments' => 'Turnieje',
+        'galery'      => 'Galeria',
+        'trainingGear' => 'Sprzęt Treningowy',
+    ],
+
+    'groups' => [
+        'youngest' => 'Skrzaty U7',
+        'young'    => 'Żaki U9',
+        'mid'      => 'Orlik U11',
+        'old'      => 'Młodzik U13',
+    ]
+];
+
